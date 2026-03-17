b387a189f6f82e4055e3275335abd93095541f6d
dibyojyoti099 <dibyojyoti562005bal@gmail.com>
2026-03-17 14:27:52 +0530

    Initial commit: add all project files

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from dotenv import load_dotenv 
+load_dotenv() 
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+genai.configure(api_key=os.getenv("api_key"))
+model = genai.GenerativeModel("gemini-2.5-flash") 
+
+def my_output(query):
+    response = model.generate_content(query) 
+    return response.text 
+
+#### UI Development using streamlit 
+
+st.set_page_config(page_title="BOT_BHAI")
+st.header("BOT_BHAI") 
+input = st.text_input("Ask Your Question:" , key = "input")  
+submit = st.button("SUBMIT") 
+
+if submit :
+    response = my_output(input) 
+    st.subheader("Answer:")
+    st.write(response)
