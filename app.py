e522460e7dd40dfb11a15d23d72b1b9f83667efa
dibyojyoti099 <dibyojyoti562005bal@gmail.com>
2026-03-18 10:07:14 +0530

    updated

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ input = st.chat_input("Ask Your Question:" , key = "input")
 #=================================================================
 if input :
     with st.chat_message(input,avatar="🧑"):
-        st.markdown(f"**{input}**")
+        st.markdown(f"***{input}***")
     response = my_output(input) 
     st.subheader("Answer:")
     st.write(response)
