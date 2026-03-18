bbd04877695676eb067ada06fbd2eda792f7cf2a
dibyojyoti099 <dibyojyoti562005bal@gmail.com>
2026-03-18 10:11:37 +0530

    updated

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def my_output(query):
 #### UI Development using streamlit 
 
 st.set_page_config(page_title="BOT_BHAI")
-st.header("Hello Dibyojyoti, I am your BOT_BHAI....")
+st.header("Hello Dibyojyoti, I'm your BOT_BHAI....")
 input = st.chat_input("Ask Your Question:" , key = "input")  
 #submit = st.button("SUBMIT") 
 #=================================================================
 if input :
     with st.chat_message(input,avatar="🧑"):
-        st.markdown(f"***{input}***")
+        st.markdown(f"**{input}**")
     response = my_output(input) 
     st.subheader("Answer:")
     st.write(response)
